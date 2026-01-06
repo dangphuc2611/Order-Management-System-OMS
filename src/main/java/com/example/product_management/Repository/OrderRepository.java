7aa6b73bf656a24d5466d5220c421831b65cb0e7
dangphuc2611 <cuccukgiontan2611@gmail.com>
2026-01-06 10:42:41 +0700

    add: custom method

--- src/main/java/com/example/product_management/Repository/OrderRepository.java
+++ src/main/java/com/example/product_management/Repository/OrderRepository.java
@@ -1,11 +1,15 @@
 package com.example.product_management.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.product_management.Entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-
+  @Query(value = "SELECT * FROM orders WHERE isdelete is :isDelete", nativeQuery = true)
+  List<Order> getOrdersByIsDelete(Boolean isDelete);
 }
